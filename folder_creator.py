68991795851e9626751113966037fb9437d29187
Dwarika Kohar <dwarikakohar790@gmail.com>
2026-01-19 10:10:54 +0545

    Fix input prompts and improve folder naming

--- folder_creator.py
+++ folder_creator.py
@@ -1,13 +1,21 @@
 import os
-a = int(input("How many file do you want from 1 to : ")
-b = input("What will be the name ex:Day ,Project etc .. : "
-def folder_creator(a,b):
-    for i in range(a):
-        os.mkdir(f"b {i}")
 
-def folder_deletor(a,b):
-    for i in range(a):
-        os.rmdir(f"b {i}")
+a = int(input("How many folders do you want from 1 to: "))
+b = input("What will be the name (e.g., Day, Project, etc.)? ")
 
-folder_creator(a,b)
-  
+def folder_creator(a, b):
+    for i in range(a):
+        folder_name = f"{b} {i}"
+        os.mkdir(folder_name)
+        print(f"Created folder: {folder_name}")
+
+def folder_deletor(a, b):
+    for i in range(a):
+        folder_name = f"{b} {i}"
+        try:
+            os.rmdir(folder_name)
+            print(f"Deleted folder: {folder_name}")
+        except OSError as e:
+            print(f"Could not delete {folder_name}: {e}")
+
+folder_creator(a, b)   
